05d16a5049592b8e8f48f6fcebae272279001491
blokh <blokh@ryed.vip>
2025-03-07 14:32:54 +0200

    resolved biome json logic

---
+++ apps/api/src/api/swagger/init-swagger.ts
@@ -0,0 +1,21 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
+
+const SWAGGER_PATH = 'docs';
+
+const SWAGGER_DOCUMENT_OPTIONS = new DocumentBuilder()
+  .setTitle('Cyolo Image System')
+  .setDescription('Cyolo Image Caching System')
+  .setVersion('1.0')
+  .build();
+
+export const SWAGGER_SETUP_OPTIONS = {
+  customCssUrl: './swagger.css',
+  customfavIcon: './favicon.png',
+  customSiteTitle: 'Cyolo Image System',
+} satisfies SwaggerCustomOptions;
+
+export const initSwagger = (app: INestApplication) => {
+  const document = SwaggerModule.createDocument(app, SWAGGER_DOCUMENT_OPTIONS);
+  SwaggerModule.setup(SWAGGER_PATH, app, document, SWAGGER_SETUP_OPTIONS);
+};
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { initSwagger } from './api/swagger/init-swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  initSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
